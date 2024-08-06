54a3f19b9d716cc4eb00ca868a9be034e2b38678
MatteoB23 <160586507+MatteoB23@users.noreply.github.com>
2024-08-06 16:08:17 +0200

    Display a selection list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {
+  NgIf,
+  NgFor,
+  UpperCasePipe,
+  CommonModule,
+} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
+  standalone: true,
+  imports: [FormsModule, UpperCasePipe]
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
